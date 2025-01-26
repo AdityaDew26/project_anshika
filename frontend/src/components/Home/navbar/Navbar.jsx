ab4f2e6e85e5b361f8da93feb792149e0bd25d2a
AdityaDew26 <songoku739529@gmail.com>
2025-01-26 18:03:34 +0530

    file added

--- frontend/src/components/Home/navbar/Navbar.jsx
+++ frontend/src/components/Home/navbar/Navbar.jsx
@@ -1,15 +1,45 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
 import "./navbar.css"
+import { useNavigate } from 'react-router-dom'
+
 
 const Navbar = () => {
+  const [scrolled, setScrolled] = useState(false);
+  const navigate = useNavigate()
+
+  const toggleLogin  = () => {
+    navigate("/logselection")
+  }
+
+   const toggleSignup  = () => {
+    navigate("/signupSelection")
+  }
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+
+
   return (
-    <div className='navbar'>
+     <div className={`navbar ${scrolled ? 'scrolled' : ''}`}>
         <div className="nav-logo">MediConnect</div>
       <div className="nav-links">
         <ul>
-            <li><button>Login</button></li>
-            <li><button>SignUp</button></li>
+            <li><button onClick={toggleLogin}>Login</button></li>
+            <li><button onClick={toggleSignup}>SignUp</button></li>
         </ul>
       </div>
     </div>
